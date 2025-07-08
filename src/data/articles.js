b2a2c6ed93fff032b25348da00ef5d1a70ac7837
stanleyshen2003 <stanley.shen2003@gmail.com>
2025-07-09 00:50:02 +0800

    merge

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,17 +1,10 @@
 import computerAssembly from "./myArticles/computerAssembly";
 import ASML from "./myArticles/asml";
-<<<<<<< HEAD
 import HCISPaper from "./myArticles/hcispaper";
-=======
->>>>>>> main
 
 
 
 
-<<<<<<< HEAD
 const myArticles = [computerAssembly, ASML, HCISPaper];
-=======
-const myArticles = [computerAssembly, ASML];
->>>>>>> main
 
 export default myArticles;
